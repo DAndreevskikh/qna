e438fffa3e22cfe2ba5612a92e78fad301bc8978
dmitry <northside753@gmail.com>
2024-07-30 01:26:20 +0300

    Improve controller tests: added validations and fixed test cases

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,54 @@
 class QuestionsController < ApplicationController
-	def index
-	  @questions = Question.all
-	end
+  helper_method :question
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = question
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+    @question = question
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def update
+    @question = question
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = question
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
