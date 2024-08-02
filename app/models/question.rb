f7b9c0663740359db109b7bf723defb943404c9b
dmitry <northside753@gmail.com>
2024-08-02 10:41:14 +0300

    Правка

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
 end
